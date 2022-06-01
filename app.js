ec457e1f41297be3e85d9dbf42ee81c3886f6338
Kashishrs <kashyapkashish22@gmail.com>
2022-06-01 23:24:23 +0530

    port

--- app.js
+++ app.js
@@ -139,6 +139,8 @@ app.use((err,req,res,next)=>{
     }
     res.status(statusCode).render('error', {err});
 })
+
+const port=process.env.PORT || 3000;
 app.listen(3000,()=>{
     console.log("hii, listening on port 3000")
 })
